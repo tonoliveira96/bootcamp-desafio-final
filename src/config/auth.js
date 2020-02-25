93035bcfb2f4b526afd34b3008547effb0768362
Everton <tonn.oliveira2011@gmail.com>
2020-02-25 12:20:03 -0300

    Autenticação com JWT

---
+++ src/config/auth.js
@@ -0,0 +1,4 @@
+export default {
+  secret: '1854e6735716045c25d4411c75bc1e9f',
+  expiresIn: '7d',
+};
--- src/routes.js
+++ src/routes.js
@@ -1,11 +1,13 @@
 import { Router } from 'express';
 
 import UserController from './app/controllers/UserController';
+import SessionController from './app/controllers/SessionController';
 
 const routes = new Router();
 
 // routes.get('/', (req, res) => res.json({ message: 'Hello Word!!!' }));
 
 routes.post('/users', UserController.store);
+routes.post('/sessions', SessionController.store);
 
 export default routes;
